622a76b2bae3f256ed52c099860921d806c5f8ad
Darren Bansil <darrenbansil@gmail.com>
2024-10-30 14:16:31 -0400

    Return preprocessed bug report and update comment regarding Terry's implementation

--- backend/preprocess_bug_report.py
+++ backend/preprocess_bug_report.py
@@ -42,9 +42,10 @@ def preprocess_bug_report(bug_report_path: str):
         print(f"Error: The bug report at '{bug_report_path}' was not found.")
         return 
 
-    # Run bug report through preprocessor [PENDING TERRY'S IMPLEMENTATION]
+    # Run bug report through preprocessor
     preprocessed_bug_report = Preprocessor.preprocess_text(bug_report_string, stop_words_path)
 
     # Apply query reformulation (MVP)
 
     # Return preprocessed bug report as a string
+    return preprocessed_bug_report
