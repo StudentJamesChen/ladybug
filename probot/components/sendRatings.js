1125114df6dca546b84a93b37e86cde675e9d8f8
patrickijieh <ijiehpatrick@gmail.com>
2024-11-13 13:21:47 -0500

    Changed messaging

--- probot/components/sendRatings.js
+++ probot/components/sendRatings.js
@@ -13,9 +13,9 @@ export const sendRatings = async(ratings, context) => {
     if(!ratingsList || ratingsList.length < 1) {
         console.error(`Ratings list is empty; sending a msg to issue #${issue.number} on ${repository.full_name}.`);
 
-        const commentBody = "Hello! LadyBug is unable to find any files that may contain the bug.\
-            \n\nIf you think this is an issue or bug, please take the time to create an issue here: [ladybug issues](https://github.com/LadyBugML/ladybug/issues/new)";
-            const issueComment = context.issue({body: commentBody});
+        const commentBody = "Hello! LadyBug was unable to find any files that might have contained the bug mentioned in this issue.\
+            \n\nIf you think this is a problem or bug, please take the time to create a bug report here: [ladybug issues](https://github.com/LadyBugML/ladybug/issues/new)";
+        const issueComment = context.issue({body: commentBody});
         try {
             await context.octokit.issues.createComment(issueComment);  
             console.log("Ratings comment was successful.");
@@ -25,7 +25,8 @@ export const sendRatings = async(ratings, context) => {
         return;
     }
 
-    let commentBody = "Hello! LadyBug was able to find and rank files that may contain the bug associated with this issue:\n";
+    let commentBody = "Hello! LadyBug was able to find and rank files that may contain the bug mentioned in this issue. \
+    \n## File ranking in order of most likely to contain the bug to least likely:\n";
     let position = 1;
 
     for (let i = 0; i < ratingsList.length; i++)
@@ -34,7 +35,8 @@ export const sendRatings = async(ratings, context) => {
     }
     
     commentBody += "\n\nPlease take the time read through each of these files. \
-    \nIf you have any problems with this response, or if you think LadyBug did a bad job, please take the time to create an issue here: [ladybug issues](https://github.com/LadyBugML/ladybug/issues/new)";
+    \nIf you have any problems with this response, or if you think an error occured, please take the time to create an issue here: [ladybug issues](https://github.com/LadyBugML/ladybug/issues/new). \
+    \nHappy coding!";
 
     console.log(`Sending the ratings to issue #${issue.number} for repository ${repository.full_name}.`);
 
@@ -44,6 +46,6 @@ export const sendRatings = async(ratings, context) => {
         await context.octokit.issues.createComment(issueComment);
         console.log("Ratings comment was successful.");
     } catch (error) {
-        console.error('Error creating rankings comment: ', error);
+        console.error('Could not create issue message: ', error);
     }
 }
